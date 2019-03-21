772dd269977b21261b737358135f48fa0f7f46d7
Chanaka Sampath Cooray <chanakasampath91@gmail.com>
2019-03-21 16:54:56 -0500

    Add summarized data

--- src/preprocessing/Summarize.py
+++ src/preprocessing/Summarize.py
@@ -34,6 +34,31 @@ def verify_headers_roots(df):
             print("roots: header: {} is not in the proper format.".format(header))
 
 
+def summarize(df, prefix_size):
+    header_list = list(df)
+
+    df_summarize = df[['OTU ID']].copy()
+
+    group_name_prev = ""
+    for header in header_list[1:]:
+        group_name_new = header[:prefix_size]
+        if group_name_new==group_name_prev:
+            continue
+        else:
+            group_name_prev = group_name_new
+
+        # spike_cols = [col for col in df.columns if group_name_new in col]
+
+        group_columns = df.filter(regex=group_name_new)
+        df_summarize["{}_sum".format(group_name_new)] = group_columns.sum(axis=1)
+        df_summarize["{}_mean".format(group_name_new)] = group_columns.mean(axis=1)
+        df_summarize["{}_non_zero".format(group_name_new)] = group_columns.astype(bool).sum(axis=1)
+
+        # print(list(group_columns))
+
+    return df_summarize
+
+
 def main():
     data_dir = "data"
     input_file1 = os.path.join(data_dir, "BG.csv")
@@ -48,6 +73,19 @@ def main():
     verify_headers_rhizo(df_rhizo)
     verify_headers_roots(df_roots)
 
+    df_BG_summarize = summarize(df_BG, 6)
+    df_rhizo_summarize = summarize(df_rhizo, 5)
+    df_roots_summarize = summarize(df_roots, 4)
+
+    output_dir = "output"
+
+    # create output directory if not exists
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    df_BG_summarize.to_csv(os.path.join(output_dir,"BG_summarize.csv"), index=None, sep=',', mode='w')
+    df_rhizo_summarize.to_csv(os.path.join(output_dir,"rhizo_summarize.csv"), index=None, sep=',', mode='w')
+    df_roots_summarize.to_csv(os.path.join(output_dir,"roots_summarize.csv"), index=None, sep=',', mode='w')
 
 if __name__ == '__main__':
     main()
